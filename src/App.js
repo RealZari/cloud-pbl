eea6f9807f79cc2482cd6cb18d45be16d0442e44
RealZari <zarizaryab2018@gmail.com>
2025-05-14 20:00:59 +0500

    Update App files in cloud-pbl submodule

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="landing-container">
+      <header className="landing-header">
+        <h1>Cloud Computing PBL</h1>
+        <p className="subtitle">Zaryab Hussain</p>
+        <p className="subtitle">Registration No: 21MDSWE158</p>
+        <div className="button-group">
+          <button className="custom-button">View Project</button>
+  <a
+  href="https://github.com/RealZari/blooddonation"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="custom-button"
+>
+  GitHub Repo
+</a>        </div>
       </header>
     </div>
   );
